a2e7ef33482531ed768d0da958458e5660fae095
GomezUndercover <S1lentD3ath02@gmail.com>
2024-10-28 09:55:06 -0600

    feat :tada: primer commit oficial

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function showChat() {
+    var x = document.getElementById("chat");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toMainUserProfile() {
+    location.href = "main-user.html";
+}
+
+function toMainPage(){
+    location.href = "index.html";
+}
+
+function toRandomPerson1(){
+    location.href = "persona-random1.html"
+}
